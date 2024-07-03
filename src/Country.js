4858cf0fd41c8f7c5deb34d57ad4a0009dea7104
shubham <shubhamchaniyara309@gmail.com>
2024-07-03 16:48:19 +0530

    final

--- src/Country.js
+++ src/Country.js
@@ -1,46 +1,3 @@
-
-// import React, { useState } from 'react';
-// import './components/country.css';
-
-// function App() {
-//   const [country, setCountry] = useState('');
-//   const [universities, setUniversities] = useState([]);
-
-//   const searchUniversities = async () => {
-//     const response = await fetch('https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json');
-//     const data = await response.json();
-//     const filtered = data.filter(university => university.country.toLowerCase() === country.toLowerCase());
-//     setUniversities(filtered);
-//   };
-
-//   return (
-//     <div className="App">
-//       <h1>Search Universities by Country</h1>
-//       <input
-//         type="text"
-//         value={country}
-//         onChange={(e) => setCountry(e.target.value)}
-//         placeholder="Enter country name"
-//       />
-//       <button onClick={searchUniversities}>Search</button>
-//       <div className="university-list">
-//         {universities.map((university, index) => (
-//           <div key={index} className="university-card">
-//             <h2>{university.name}</h2>
-//             <a href={`http://${university.web_pages[0]}`} target="_blank" rel="noopener noreferrer">
-//               {university.web_pages[0]}
-//             </a>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 import React, { useState, useEffect } from 'react';
 import './components/country.css';
 
@@ -117,3 +74,6 @@ function App() {
 }
 
 export default App;
+
+
+
